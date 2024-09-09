3c81ac7c277f022700fb77f01c2378fb11cb4307
Erwinn7 <ahissouanselme14@gmail.com>
2024-09-09 14:25:46 +0200

    apres revu de code

--- Atelier 2/Exo 3.py
+++ Atelier 2/Exo 3.py
@@ -1,3 +1,29 @@
+# def separate(lst:list)->list:
+#     separatedList = []
+#     for elt in sorted(lst):
+#         separatedList.append(elt)
+#     return separatedList
+# print(separate([1,5,3,0,-4,-9,0,-1,10]))
+
+# def separate(lst:list)->list:
+#     """Renvoie la liste entrée avec ses entiers positifs et négatifs séparés"""
+#     separatedList= []
+#     for elt in lst:
+#         if elt < 0:
+#             separatedList.insert(0,elt)
+#         else:
+#             separatedList.append(elt)
+#     return separatedList
+
+# print(separate([1,5,3,0,-4,-9,0,-1,10]))
+# print('Liste vide : ', separate([]))
+
+
+
+
+
+
+
 def separer(lstEntier:list)-> list:
     """    Sépare les entiers positifs et négatifs dans une liste.
 
@@ -18,20 +44,29 @@ def separer(lstEntier:list)-> list:
         liste n'est pas garanti d'être le même que dans la liste d'origine. """
 
 
+    # longueur de la liste
     lenLstEntier = len(lstEntier)
     positiveIndex = lenLstEntier -1
     negativeIndex = 0
     # on initialise la liste avec des 0 comme c'est une liste d'entier et la liste a la même taille que lstEntier
-    LstLSEP = [0]*lenLstEntier
+    lstLSEP = [0]*lenLstEntier
 
     for elt in lstEntier:
+        # si le nombre est négatif : on le rajoute au début de la liste
         if elt < 0 :
-            LstLSEP[negativeIndex] = elt
+            lstLSEP[negativeIndex] = elt
             negativeIndex += 1
+        # si le nombre est positif : on le rajoute à la fin de la liste
         elif elt > 0 :
-            LstLSEP[positiveIndex] = elt
+            lstLSEP[positiveIndex] = elt
             positiveIndex -= 1
+        
 
-    return LstLSEP
+    return lstLSEP
+
+
+###TEST a reprendre avec une fonction 
 
 print(separer([3, -2,0, -1, 5, -3, 4,0,0]))
+print(separer([]))
+print(separer([1,5,2,0]))
